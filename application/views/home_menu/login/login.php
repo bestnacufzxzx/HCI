fb7017afbf9bac1162672d927de02769d936a924
Wichuta18 <slotsnow0301@gmail.com>
2019-05-16 22:12:59 +0700

    edit login

--- application/views/home_menu/login/login.php
+++ application/views/home_menu/login/login.php
@@ -1,7 +1,5 @@
-
+<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"> -->
 <body>
-
-	
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('public/img/bg-01.jpg');">
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
@@ -9,18 +7,14 @@
 					<span class="login100-form-title p-b-49">
 						Login
 					</span>
-
-					<div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
-						<span class="label-input100">Username</span>
-						<input class="input100" type="text" name="username" placeholder="Type your username">
-						<span class="focus-input100" data-symbol="&#xf206;"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<span class="label-input100">Password</span>
-						<input class="input100" type="password" name="pass" placeholder="Type your password">
-						<span class="focus-input100" data-symbol="&#xf190;"></span>
-					</div>
+						<div class="wrap-input100  mb-3">
+							<span class="label-input100">ชื่อผู้ใช้</span>
+							<input  class="input100" type="text" id="username" name="username"  placeholder="ชื่อผู้ใช้">
+						</div>
+						<div class="wrap-input100  mb-2">
+							<span class="label-input100">รหัสผ่าน</span>
+							<input  class="input100" type="password" id="password" name="password"  placeholder="รหัสผ่าน">
+						</div>
 					
 					<div class="text-right p-t-8 p-b-31">
 						<a href="#">
@@ -31,38 +25,12 @@
 					<div class="container-login100-form-btn">
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
-							<button class="login100-form-btn">
+							<button onclick="checklogin()" class="login100-form-btn">
 							<a href="home">Login
 							</button>
 						</div>
-					</div>
-
-					<div class="txt1 text-center p-t-54 p-b-20">
-						<span>
-							Or Sign Up Using
-						</span>
-					</div>
-
-					<div class="flex-c-m">
-						<a href="#" class="login100-social-item bg1">
-							<i class="fa fa-facebook"></i>
-						</a>
-
-						<a href="#" class="login100-social-item bg2">
-							<i class="fa fa-twitter"></i>
-						</a>
-
-						<a href="#" class="login100-social-item bg3">
-							<i class="fa fa-google"></i>
-						</a>
-					</div>
-
-					<div class="flex-col-c p-t-155">
-						<span class="txt1 p-b-17">
-							Or Sign Up Using
-						</span>
-
-						<a href="#" class="txt2">
+						<div class="flex-col-c p-t-155">
+						<a href="register" class="txt2">
 							Sign Up
 						</a>
 					</div>
@@ -72,7 +40,7 @@
 	</div>
 	
 
-	<div id="dropDownSelect1"></div>
+	<!-- <div id="dropDownSelect1"></div> -->
 	
 
 	
@@ -80,3 +48,19 @@
 </body>
 </html>
 
+<script>
+
+    function checklogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username == "group3" && password == "1234"){
+		alert("ล็อกอินสำเร็จ  "+username)
+        window.location.href = 'home';
+
+    }
+    else{
+        alert("ล็อกอินไม่สำเร็จ")
+    }
+ 
+}
+</script>
