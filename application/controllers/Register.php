63e8149e2367dca0c8baf38bb2e317baaf299fd5
Wichuta18 <slotsnow0301@gmail.com>
2019-05-16 22:29:14 +0700

    create script

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -6,11 +6,10 @@ class Register extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('template/headhome');
-		$this->load->view('template/headregister');
+		$this->load->view('template/headerlogin');
 		$this->load->view('home_menu/register/register');
 		$this->load->view('home_menu/register/script');
-		$this->load->view('template/footerhome');
+		$this->load->view('template/footerlogin');
 
 	}
 }
