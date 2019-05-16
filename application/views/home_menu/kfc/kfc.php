5de46324e2de1b94b46cb396bc9a218342948cb7
Puth1998 <Teenaputh12@hotmail.com>
2019-05-16 21:26:33 +0700

    commit delivery

--- application/views/home_menu/kfc/kfc.php
+++ application/views/home_menu/kfc/kfc.php
@@ -1,48 +1,61 @@
 <body>    
-    <section class="site-hero overlay page-inside" style="background-image: url(public/img/prop_single_img.jpg)">
+    <section class="site-hero overlay page-inside" style="background-image: url(public/img/de.png)"> 
       <div class="container">
         <div class="row site-hero-inner align-items-center">
           <div class="col-md-7 text-left mr-auto" data-aos="fade-up">
-            <h1 class="heading">$1,650</h1>
-            <p class="sub-heading">2724 20th Ave N, Houston TX</p>
+            <h5 class="heading">Food Delivery</h5>
+            <p class="sub-heading">It's the food you love, delivered</p>
           </div>
         </div>
         <a href="#next-section" class="smoothscroll scroll-down">Scroll Down</a>
-      </div>
+      </div> 
     </section>
     <!-- END section -->
 
 
-    <section class="section" id="next-section">
+    <section class="section visit-section" id="next-section" >
       <div class="container">
-        <div class="row mb-5">
-          <div class="col-md-6 mb-4 mb-md-0">
-            <img src="public/img/img_1.jpg" alt="Image" class="img-fluid rounded img-shadow">
-          </div>
-          <div class="col-md-6">
-            <img src="public/img/img_2.jpg" alt="Image" class="img-fluid rounded img-shadow">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="heading" data-aos="fade-up">Popular Deliverys</h2>
           </div>
         </div>
-        <div class="row mt-5">
-          
-          <div class="col-lg-8 ml-auto">
-            <h3 class="mb-3">$1,650</h3>
-            <p class="mb-5">2724 20th Ave N, Houston TX</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe omnis beatae libero quisquam ex nostrum repellendus, consectetur suscipit. Velit iusto ducimus sit quos officiis nesciunt libero, officia, aliquam doloremque totam.</p>
-            <p>Ratione natus expedita iusto, minus cumque vitae quo culpa reiciendis aspernatur aliquam veritatis magnam non, dicta nemo inventore, nisi quas dolores. Modi laboriosam sunt aliquid rem, deserunt quis? Porro, hic.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe omnis beatae libero quisquam ex nostrum repellendus, consectetur suscipit. Velit iusto ducimus sit quos officiis nesciunt libero, officia, aliquam doloremque totam.</p>
-            <p>Ratione natus expedita iusto, minus cumque vitae quo culpa reiciendis aspernatur aliquam veritatis magnam non, dicta nemo inventore, nisi quas dolores. Modi laboriosam sunt aliquid rem, deserunt quis? Porro, hic.</p>
-
-            <p><a href="contact.html" class="btn btn-primary">Contact Me</a></p>
-            
+        <div class="row">
+          <div class="col-lg-3 col-md-6 visit mb-3" data-aos="fade-up">
+            <a href="https://www.kfc.co.th/menu/promotions?utm_source=google&utm_medium=sem&utm_campaign=180517_always-on_sem&utm_content=always-on_sem&gclid=CjwKCAjwlPTmBRBoEiwAHqpvhXNd3OVn9Z0lJ2BoNy_YuahE_zLMt2-mMdHOT7ijUwh5uqD7JzM1-hoCWMgQAvD_BwE&fbclid=IwAR0hrgRkOaaMPTXmNLjuxkqyVmlALSGVimKOPxhogu-Hs9JpAZHx-8HD3nI"><img src="public/img/kfc.jpg" alt="Image placeholder" class="img-fluid rounded"> </a>
+            <h3><a href="https://www.kfc.co.th/menu/promotions?utm_source=google&utm_medium=sem&utm_campaign=180517_always-on_sem&utm_content=always-on_sem&gclid=CjwKCAjwlPTmBRBoEiwAHqpvhXNd3OVn9Z0lJ2BoNy_YuahE_zLMt2-mMdHOT7ijUwh5uqD7JzM1-hoCWMgQAvD_BwE&fbclid=IwAR0hrgRkOaaMPTXmNLjuxkqyVmlALSGVimKOPxhogu-Hs9JpAZHx-8HD3nI">KFC</a></h3>
+            <div class="reviews-star float-left">
+            Fast Food Restaurant 
+            </div>
           </div>
+          <div class="col-lg-3 col-md-6 visit mb-3" data-aos="fade-up" data-aos-delay="100">
+            <a href="https://www.mcdonalds.co.th/mcDelivery/nonMember?fbclid=IwAR2bIeOF_t7ygDyWXEAZw00idLTWc-2Y6HOqG7CUTCh-jim0heG8Z8kT23Y"><img src="public/img/mc.png" alt="Image placeholder" class="img-fluid rounded"> </a>
+            <h3><a href="https://www.mcdonalds.co.th/mcDelivery/nonMember?fbclid=IwAR2bIeOF_t7ygDyWXEAZw00idLTWc-2Y6HOqG7CUTCh-jim0heG8Z8kT23Y">Mcdonald</a></h3>
+            <div class="reviews-star float-left">
+            Burger; American; Delivery            
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 visit mb-3" data-aos="fade-up" data-aos-delay="200">
+            <a href="https://www.pizzahut.co.th/menu/pizza?fbclid=IwAR2t3BUc8wYslTuTpGk5D42Ijmsc_0wj03jN5pmox9Hept6EuDqBqOgYzyM"><img src="public/img/Pizza_Hut.png" alt="Image placeholder" class="img-fluid rounded"> </a>
+            <h3><a href="https://www.pizzahut.co.th/menu/pizza?fbclid=IwAR2t3BUc8wYslTuTpGk5D42Ijmsc_0wj03jN5pmox9Hept6EuDqBqOgYzyM">Pizza Hut</a></h3>
+            <div class="reviews-star float-left">
+            Pizza Hut Call 1150
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 visit mb-3" data-aos="fade-up" data-aos-delay="300">
+            <a href="https://www.1112.com/?fbclid=IwAR3mPmQDhvVUbvJySA0UqFlIFOtgpa4Lh38_UoO-ucIn8o7nmi_nKmg8jFw"><img src="public/img/pizza_com.jpg" alt="Image placeholder" class="img-fluid rounded"> </a>
+            <h3><a href="https://www.1112.com/?fbclid=IwAR3mPmQDhvVUbvJySA0UqFlIFOtgpa4Lh38_UoO-ucIn8o7nmi_nKmg8jFw">Pizza Company</a></h3>
+            <div class="reviews-star float-left">   
+            The Pizza Company 1112
+            </div>
+          </div>
+
         </div>
       </div>
     </section>
 
 
-    <section class="section">
+    <!-- <section class="section">
       <div class="container">
         <div class="row">
           <div class="col-lg-7">
@@ -57,7 +70,7 @@
           </div>
         </div>
       </div>
-    </section>
+    </section> -->
 
   </body>
 </html>
