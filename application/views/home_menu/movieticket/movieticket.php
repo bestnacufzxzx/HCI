9517333fcba7a13f4b22fb98da01b7843f602c91
Puth1998 <Teenaputh12@hotmail.com>
2019-05-16 23:38:21 +0700

    movie

--- application/views/home_menu/movieticket/movieticket.php
+++ application/views/home_menu/movieticket/movieticket.php
@@ -1,5 +1,5 @@
 <body>    
-    <section class="site-hero overlay page-inside" style="background-image: url(public/img/prop_single_img.jpg)">
+    <section class="site-hero overlay page-inside" style="background-image: url(public/img/res-cinema-01.jpg)">
       <div class="container">
         <div class="row site-hero-inner align-items-center">
           <div class="col-md-7 text-left mr-auto" data-aos="fade-up">
@@ -13,17 +13,19 @@
     <!-- END section -->
 
 
-    <section class="section" id="next-section">
+    <section class="section visit-section" id="next-section">
       <div class="container">
         <div class="row mb-5">
-          <div class="col-md-6 mb-4 mb-md-0">
-            <img src="public/img/img_1.jpg" alt="Image" class="img-fluid rounded img-shadow">
+          <div class="col-md-6 mb-4 visit mb-md-0">
+          <a href="Mobilemoney"><img src="public/img/sf.jpg" alt="Image placeholder" class="img-fluid rounded"> </a>
+            <h3><a href="Mobilemoney">SF</a></h3>
           </div>
-          <div class="col-md-6">
-            <img src="public/img/img_2.jpg" alt="Image" class="img-fluid rounded img-shadow">
+          <div class="col-md-6 mb-4 visit mb-md-0">
+          <a href="Mobilemoney"><img src="public/img/mj2.png" alt="Image placeholder" class="img-fluid rounded"> </a>
+            <h3><a href="Mobilemoney">Major</a></h3>
           </div>
         </div>
-        <div class="row mt-5">
+        <!-- <div class="row mt-5">
           
           <div class="col-lg-8 ml-auto">
             <h3 class="mb-3">$1,650</h3>
@@ -37,27 +39,11 @@
             <p><a href="contact.html" class="btn btn-primary">Contact Me</a></p>
             
           </div>
-        </div>
+        </div> -->
       </div>
     </section>
 
-
-    <section class="section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-7">
-            <img src="public/img/about_1.jpg" alt="Image" class="img-fluid rounded img-shadow">
-          </div>
-          <div class="col-lg-4 ml-auto">
-            <h3 class="mb-3">Contact Agent</h3>
-            <p>I'm John Doe a realtor agent, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe omnis beatae libero quisquam ex nostrum repellendus, consectetur suscipit. Velit iusto ducimus sit quos officiis nesciunt libero, officia, aliquam doloremque totam.</p>
-            <p>Ratione natus expedita iusto, minus cumque vitae quo culpa reiciendis aspernatur aliquam veritatis magnam non, dicta nemo inventore, nisi quas dolores. Modi laboriosam sunt aliquid rem, deserunt quis? Porro, hic.</p>
-            <p><a href="contact.html" class="btn btn-primary">Contact Me</a></p>
-            
-          </div>
-        </div>
-      </div>
-    </section>
+    <!-- END section -->
 
   </body>
 </html>
