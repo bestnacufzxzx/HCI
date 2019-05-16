fedd2d577d683386fcaa5284dbe7b1066d48a813
namfon317 <mdjdjm317@gmail.com>
2019-05-16 22:44:36 +0700

    Edit contact

--- application/views/contact.php
+++ application/views/contact.php
@@ -1,5 +1,5 @@
-  <body>
-    <section class="site-hero overlay page-inside" style="background-image: url(public/img/hero_1.jpg)">
+<body>
+    <section class="site-hero overlay page-inside" style="background-image: url(public/img/bg-01.jpg)">
       <div class="container">
         <div class="row site-hero-inner align-items-center">
           <div class="col-md-7 text-left mr-auto">
@@ -11,34 +11,34 @@
     </section>
     <!-- END section -->
 
-
-    <section class="section bg-primary contact-section" id="next-section">
-      <div class="container">
+  
+    <section class="section contact-section" id="next-section" style="background-image: url('public/img/bg-01.jpg');">
+      <div class="container"  >
         <div class="row">
           <div class="col-md-7">
             
             <form action="#" method="post" class="bg-white p-md-5 p-4 mb-5" style="margin-top: -150px;">
               <div class="row">
                 <div class="col-md-6 form-group">
-                  <label for="name">Name</label>
-                  <input type="text" id="name" class="form-control ">
+                  <label for="name">Name :</label>
+                  <input type="text" id="name" placeholder="Sirirat" class="form-control ">
                 </div>
                 <div class="col-md-6 form-group">
-                  <label for="phone">Phone</label>
-                  <input type="text" id="phone" class="form-control ">
+                  <label for="phone">Phone :</label>
+                  <input type="text" id="phone" placeholder="0812345678" class="form-control ">
                 </div>
               </div>
           
               <div class="row">
                 <div class="col-md-12 form-group">
-                  <label for="email">Email</label>
-                  <input type="email" id="email" class="form-control ">
+                  <label for="email">Email :</label>
+                  <input type="email" id="email" placeholder="fakpay@gmail.com" class="form-control ">
                 </div>
               </div>
               <div class="row">
                 <div class="col-md-12 form-group">
-                  <label for="message">Write Message</label>
-                  <textarea name="message" id="message" class="form-control " cols="30" rows="8"></textarea>
+                  <label for="message">Write Message :</label>
+                  <textarea name="message" id="message" placeholder="hello..." class="form-control " cols="30" rows="8"></textarea>
                 </div>
               </div>
               <div class="row">
@@ -52,9 +52,9 @@
           <div class="col-md-5">
             <div class="row">
               <div class="col-md-10 ml-auto contact-info">
-                <p><span class="d-block">Address:</span> <span> 98 West 21th Street, Suite 721 New York NY 10016</span></p>
-                <p><span class="d-block">Phone:</span> <span> (+1) 435 3533</span></p>
-                <p><span class="d-block">Email:</span> <span> info@yourdomain.com</span></p>
+                <p><span class="d-block">Address:</span> <span> Walailak University     <br>Nakhon Si Thammarat 80161</span></p>
+                <p><span class="d-block">Phone:</span> <span> 06-6666666</span></p>
+                <p><span class="d-block">Email:</span> <span> fakpay@gmail.com</span></p>
               </div>
             </div>
           </div>
