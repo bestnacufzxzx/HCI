3e8ce528456d1ef6b8e59d821abc2e3ab350f2ac
bestnacufzxzx <bestnacufzxzx1@gmail.com>
2019-05-20 14:26:58 +0700

    edit page qr code

--- application/views/home_menu/barcode/barcode.php
+++ application/views/home_menu/barcode/barcode.php
@@ -1,10 +1,10 @@
   <body>    
-    <section class="site-hero overlay page-inside" style="background-image: url(public/img/prop_single_img.jpg)">
+  <section class="site-hero overlay page-inside" style="background-image: url(public/img/PageQR/QRCodeQ.jpg)">
       <div class="container">
         <div class="row site-hero-inner align-items-center">
           <div class="col-md-7 text-left mr-auto" data-aos="fade-up">
-            <h1 class="heading">$1,650</h1>
-            <p class="sub-heading">2724 20th Ave N, Houston TX</p>
+            <!-- <h1 class="heading">$1,650</h1>
+            <p class="sub-heading">2724 20th Ave N, Houston TX</p> -->
           </div>
         </div>
         <a href="#next-section" class="smoothscroll scroll-down">Scroll Down</a>
@@ -15,7 +15,7 @@
 
     <section class="section" id="next-section">
       <div class="container">
-        <div class="row mb-5">
+        <!-- <div class="row mb-5">
           <div class="col-md-6 mb-4 mb-md-0">
             <img src="public/img/img_1.jpg" alt="Image" class="img-fluid rounded img-shadow">
           </div>
@@ -37,12 +37,59 @@
             <p><a href="contact.html" class="btn btn-primary">Contact Me</a></p>
             
           </div>
+        </div> -->
+
+        <div class="card">
+        <div class="card-header" align="center">Money balance จำนวนเงินคงเหลือ : 10,000 ฿
+          <!-- <button type="button" onclick="alert('Hello world!')">create QR and Barcode</button> -->
+          <div class="container">
+            <!-- <h2>Modal Example</h2> -->
+            <!-- Trigger the modal with a button -->
+            <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">create QR and Barcode</button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" role="dialog">
+              <div class="modal-dialog">
+              
+                <!-- Modal content-->
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <!-- <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Modal Header</h4> -->
+                  </div>
+                  <div class="modal-body">
+                    <p>10,000 ฿</p>
+                    <img src="public/img/PageQR/meny10000.png" alt="Image" class="img-fluid rounded img-shadow">
+                   <br> <br> <br>
+                    <div class="card">
+                    <img src="public/img/PageQR/barcode.png" alt="Image" class="img-fluid rounded img-shadow">
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+            
+          </div>
+          <!-- <a>create QR and Barcode</a>  -->
         </div>
-      </div>
+        <!-- <div class="card-body" align="center">ScanQRcode
+          <div class="card">
+          <div class="card-header" align="center">Money balance จำนวนเงินคงเหลือ : </div>
+
+          </div> 
+        </div> 
+        <div class="card-footer" align="center">San Barcode </div>
+      </div> -->
+
+    </div>
     </section>
 
 
-    <section class="section">
+    <!-- <section class="section">
       <div class="container">
         <div class="row">
           <div class="col-lg-7">
@@ -57,7 +104,7 @@
           </div>
         </div>
       </div>
-    </section>
+    </section> -->
 
   </body>
 </html>
